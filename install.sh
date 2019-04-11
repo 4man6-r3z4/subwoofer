575e5c4479541e828cd74c5dbf1a83e83c648d4b
KemangClub <49513707+KemangClub@users.noreply.github.com>
2019-04-12 00:14:15 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg update -y
+pkg upgrade -y
+pkg install python2 -y
+pkg install python -y
+pkg install git -y
+pkg install termux-api -y
+pkg install php -y
+pkg install ruby -y
+pkg install -y 
---
+++ subwoofer.sh
@@ -0,0 +1,71 @@
+#!/system/xbin/bash
+#Author InYouDfv47
+#Selamat Mencoba
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+
+echo $white 
+cowsay -f eyes Mr.r3z4 X SUBWOOFER
+figlet -f slant Mr.r3z4
+
+echo $green
+read -p "╭─[ USERNAME ]
+╰─•>" user
+
+echo $green
+read -p "╭─[ PASSWORD ]
+╰─•>" pass
+
+if [ Mr.r3z4 = $user ] | [ $user = Mr.r3z4 ]
+then
+echo $green "Username Benar..."
+else
+echo $green "Username Salah..."
+exit
+fi
+
+if [ Anonymous = $pass ] | [ $pass = Anonymous ]
+then
+echo $green "Password Benar..."
+else
+echo $green "Password Salah..."
+exit
+fi
+
+echo $white
+clear
+figlet -f slant SUBWOOFER
+echo $green "HALO.... SELAMAT DATANG DI TOOLS KAMI"
+
+echo $purple"==========================================="
+echo $purple"= [01] Facebook Brute Force               ="
+echo $purple"= [02] Instagram Brute Force              ="
+echo $purple"= [03] Gmail Brute Force                  ="
+echo $purple"= [04] Multi Brute Force                  ="
+echo $purple"= [05] SnapChat Brute Force               ="
+echo $purple"= [06] Twitter Brute Force                ="
+echo $purple"= [07] ===================================="
+echo $purple"= [08] ="
+echo $purple"= [09] ="
+echo $purple"= [10] ="
+echo $purple"= [11] ="
+echo $purple"= [12] ="
+echo $purple"= [13] ="
+echo $purple"= [14] ="
+echo $purple"= [15] ="
+echo $purple"= [16] ="
+echo $purple"= [17] ="
+echo $purple"= [18] ="
+echo $purple"= [19] ="
+echo $purple"= [20] ="
+echo $purple"= [00] ="
+echo $purple"=========================================="
+echo $green
+read -p "╭─[ PILIH NOMOR ]
+╰─•>" no
